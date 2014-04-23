76e3e8d0025f9e371b7ddfc6de3a69d4fccbfeed
tornado2 <aksharsarvesh@yahoo.com>
2014-04-23 16:01:48 -0700

    pythonroom update

--- turtledraw.py
+++ turtledraw.py
@@ -1,1 +1,11 @@
 # author: tornado2
+import turtle
+akshar = turtle.Turtle()
+
+length = 100
+angle = 90
+count = 0
+while count < 4:
+	akshar.forward(length)
+	akshar.right(angle)
+	count = count + 1
