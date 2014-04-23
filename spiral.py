caf00b9a5ee5b6130a0d2378d5a7f5d63be80551
tornado2 <aksharsarvesh@yahoo.com>
2014-04-23 16:57:34 -0700

    pythonroom update

--- spiral.py
+++ spiral.py
@@ -1,1 +1,11 @@
 # author: tornado2
+import turtle
+akshar = turtle.Turtle()
+length = 10
+sides = 0
+while sides < 50:
+	akshar.forward(length)
+	akshar.left(175)
+	sides = sides + 1
+	length = length + 1
+
