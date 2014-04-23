c7d6402d9369146e40b74748741191ffec363cab
tornado2 <aksharsarvesh@yahoo.com>
2014-04-23 16:51:08 -0700

    pythonroom update

--- turtledraw.py
+++ turtledraw.py
@@ -1,13 +1,17 @@
 # author: tornado2
 import turtle
 akshar = turtle.Turtle()
-sides = 6
-length = 100
+sides = 150
+length = 10
 angle = 180 - 180 * (sides - 2)/sides
-
+numbers = range(0,sides)
 colors = ["red","orange","yellow","green","blue","purple"]
+for number in numbers:
+	for color in colors:
+		akshar.color(color)
+		akshar.forward(length)
+		akshar.right(angle +200)
+	
+	
 
-for color in colors:
-	akshar.color(color)
-	akshar.forward(length)
-	akshar.right(angle)
+
