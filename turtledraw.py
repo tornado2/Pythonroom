3dd427c572b08f428434bd9265b7153370f872b1
tornado2 <aksharsarvesh@yahoo.com>
2014-04-23 16:16:12 -0700

    pythonroom update

--- turtledraw.py
+++ turtledraw.py
@@ -1,11 +1,10 @@
 # author: tornado2
 import turtle
 akshar = turtle.Turtle()
-
+sides = 5
 length = 100
-angle = 90
-count = 0
-while count < 4:
+angle = 180 - 180 * (sides - 2)/sides
+numbers = [ 1, 2, 3, 4, 5, 6 ]
+for number in numbers:
 	akshar.forward(length)
 	akshar.right(angle)
-	count = count + 1
