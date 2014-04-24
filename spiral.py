fad73b82230880b5171122ea1e568823d3070bff
tornado2 <aksharsarvesh@yahoo.com>
2014-04-23 17:02:21 -0700

    pythonroom update

--- spiral.py
+++ spiral.py
@@ -3,9 +3,9 @@ import turtle
 akshar = turtle.Turtle()
 length = 10
 sides = 0
-while sides < 50:
+while sides < 200:
 	akshar.forward(length)
-	akshar.left(175)
+	akshar.left(50)
 	sides = sides + 1
 	length = length + 1
 
