089fb3c8945587e6cfbfeb5690157747a50bea47
tornado2 <aksharsarvesh@yahoo.com>
2014-04-30 17:25:03 -0700

    pythonroom update

--- numbers.py
+++ numbers.py
@@ -1,7 +1,13 @@
 # author: tornado2
-numbers = (3)
-while numbers < 103:
-	print(numbers)
-	numbers = numbers + 3
-	if numbers == 102:
-		numbers = 103
+numbers = range (1, 101)
+numb = 0
+
+for number in numbers:
+	
+	if number % 5 == 0:
+		
+	
+	
+	    numb = numb + number
+	
+print numb	
