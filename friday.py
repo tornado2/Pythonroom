04fc65a23686f7e9e18e5740d13bd39d8f469c58
tornado2 <aksharsarvesh@yahoo.com>
2014-04-30 17:59:02 -0700

    pythonroom update

--- friday.py
+++ friday.py
@@ -5,4 +5,20 @@ year = 1900
 total = 0
 while year < 2015:
 	total = total + 1
+	if month = 2:
+		if day = 28:
+			day = 1
+			month = month + 1
+	if month = 1 or month = 3 or month = 5 or month = 7 or month = 8 or month = 10 or month = 12:
+		if day = 31:
+			day = 1
+			month = month + 1
+			if month = 13:
+				month = 1
+		if month = 4 or month = 6 or month = 9 or month + 11:
+			if day = 30:
+				day = 1
+				month = month + 1
+			
+		
 print str(total) + " days."	
