ce6ab224b4bf05ed66423a2c684de747662e4fc4
tornado2 <aksharsarvesh@yahoo.com>
2014-05-07 16:18:56 -0700

    pythonroom update

--- cleverbot.py
+++ cleverbot.py
@@ -1,3 +1,21 @@
 # author: tornado2
 answer = input ("HELLO")
-print answer
+answer = answer.lower()
+word = answer.split()
+if word[0] = "how":
+	if word[1] = "do":
+		if word[2] = "you":
+			if word[3] = "do":
+				print "I'm fine"
+	if word[1] = "are":
+		if word[2] = "you":
+			if word[3] = "a":
+				if word[4] = "robot":
+					print "I am not a robot. I am THE Cleverbot 3000"
+				
+					
+					
+					
+	
+	
+
