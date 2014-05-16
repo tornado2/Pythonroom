20e9038f0d49c9e09a7dc52731d86b4216f19beb
tornado2 <aksharsarvesh@yahoo.com>
2014-05-15 18:25:07 -0700

    pythonroom update

--- Lightswitch.py
+++ Lightswitch.py
@@ -1,28 +1,11 @@
 # author: tornado2
-day = 1
-month = 1
-year = 1900
-total = 0
-while year < 2015:
-	total = total + 1
-	if month == 2:
-		if day == 28:
-			day = 1
-			month = month + 1
-		else day = day + 1
-	if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
-		if day == 31:
-			day = 1
-			month = month + 1
-		else day = day + 1	
-			if month == 13:
-				month = 1
-				year = year + 1
-		if month == 4 or month == 6 or month == 9 or month == 11:
-			if day == 30:
-				day = 1
-				month = month + 1
-			else day = day + 1
-			
-		
-print str(total) + " days."	
+People = range (1,101)
+switch = false
+rounds = (1,101)
+for r in rounds:
+	for p in people:
+		if p % r = 0:
+			if switch = false:
+				switch = true
+			else switch = false
+print switch
