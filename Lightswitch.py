2cee450ffb98a842ce8c96f99b333a2cad2515bc
tornado2 <aksharsarvesh@yahoo.com>
2014-05-21 15:31:58 -0700

    pythonroom update

--- Lightswitch.py
+++ Lightswitch.py
@@ -1,11 +1,12 @@
 # author: tornado2
 people = range (1,101)
-switch = false
+switch = False
 Round = (1,101)
 for r in Round:
 	for p in people:
-		if p % r = 0:
-			if switch = false:
-				switch = true
-			else switch = false
+		if p % r == 0:
+			if switch == False:
+				switch = True
+			else:
+				switch = False
 print switch
